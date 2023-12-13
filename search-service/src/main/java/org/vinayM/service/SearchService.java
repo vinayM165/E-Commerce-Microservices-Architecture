534ee297c1efd0040f100097fd2e0b71650b71d3
Vinay Mandge <59912201+vinayM165@users.noreply.github.com>
2023-12-13 11:57:59 +0530

    - search-service : 0.1 1. Added to eureka client and API Gateway 2. Prototype requirements done.

--- search-service/src/main/java/org/vinayM/service/SearchService.java
+++ search-service/src/main/java/org/vinayM/service/SearchService.java
@@ -1,23 +1,39 @@
 package org.vinayM.service;
 
+
+import co.elastic.clients.elasticsearch.core.*;
+import co.elastic.clients.elasticsearch.core.search.Hit;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.vinayM.model.Product;
 import org.vinayM.repository.SearchRepository;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+import static java.util.Collections.singletonMap;
+import static org.springframework.expression.spel.SpelCompilerMode.IMMEDIATE;
+
 @Service
 public class SearchService {
+    @Autowired
+    RestHighLevelClient highLevelClient;
 
+    private final String indexName = "products";
     @Autowired
     SearchRepository repository;
     public List<Product> getAll(){
         List<Product> li = StreamSupport.stream(repository.findAll().spliterator(), false)
                 .collect(Collectors.toList());
         return li;
+    }
+
+    public List<Product> findByName(String name) {
+        return repository.findByName(name);
     }
 }
